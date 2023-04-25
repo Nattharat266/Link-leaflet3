7e4460acb5dbe8756566b442e1893a7a0f2b9be9
Nattharat266 <124670881+Nattharat266@users.noreply.github.com>
2023-04-25 15:57:49 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,237 @@
+ //add data
+//  var tourpoint = {
+//     "type": "FeatureCollection",
+//     "features": [
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "bangkok",
+//           "type": "special",
+//           "level": 10
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.49105977897949,
+//             13.761729384836102
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "pathumthani",
+//           "type": "specail",
+//           "level": 10
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.53548098060782,
+//             14.023660292803953
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "nonthaburi",
+//           "type": "special",
+//           "level": 10
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.49245132599242,
+//             13.841821809994457
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "samutprakan",
+//           "type": "culture",
+//           "level": 9
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.60088222691212,
+//             13.595974380694614
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "nakhonpathom",
+//           "type": "special",
+//           "level": 9
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.0615549419623,
+//             13.81938755634296
+//           ],
+//           "type": "Point"
+//         }
+//       }
+//     ]
+//   }
+
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะช้าง",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.33858609240275,
+            12.044303398688541
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะหมาก",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.48127501225525,
+            11.817238986027846
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะกูด",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.57104550813972,
+            11.657471003830878
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะกระดาด",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.52706407946386,
+            11.840846251888195
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะหวาย",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.40461881407276,
+            11.901229944225932
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดริมน้ำวัดไทรทอง",
+          "type": "กิจกรรมพิเศษ",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.4787605281017,
+            12.201423125769551
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ชุมชนบ้านน้ำเชี่ยว",
+          "type": "วัฒนธรรม",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            102.45961910694638,
+            12.190124471834707
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะขาม",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.46009656863589,
+            11.83435831605729
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสละสมโภชน์จังหวัดตราด",
+          "type": "กิจกรรมพิเศษ",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.46849490359983,
+            12.287441857892489
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะรัง",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.38654524158068,
+            11.796133511005209
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
